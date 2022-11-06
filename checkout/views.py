5b39156b580cb934eb8fae6c56ca832aafe8c2f6
Colin Shaw <colin.shaw400@outlook.com>
2022-11-06 17:38:25 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M1CYYGLT9eFji1UAXkz1tcBoXIzozeKbxP3j2Tf31pRfj8qD2bcczgQjwfMCQEC3el0XZYIY4CBY7oaRKvaXp9w00XkO37pJh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
